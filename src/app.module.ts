f644b0a4d329bcdff4905a3e4261db8780df6ebd
MaksGuz <maax.guzmaan@gmail.com>
2024-09-13 22:23:11 -0600

    DG BD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import {TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmployeesModule } from './employees/employees.module';
+import { ProductsModule } from './products/products.module';
+import { ConfigModule } from '@nestjs/common';
 
 @Module({
-  imports: [EmployeesModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+          host: process.env.host,
+          port: +process.env.port,
+          username: 'postgres',
+          password: process.env.pass,
+          database: process.env.name,
+          entities: [],
+          autoLoadEntities: true,
+          synchronize: true,
+  }), 
+  EmployeesModule, 
+  ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
