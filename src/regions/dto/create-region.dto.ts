1714b2ccc2ff49092028d2d558c768e92756939a
MaksGuz <maax.guzmaan@gmail.com>
2024-09-26 20:23:02 -0600

    Manager and Regions setup

--- src/regions/dto/create-region.dto.ts
+++ src/regions/dto/create-region.dto.ts
@@ -1,1 +1,11 @@
-export class CreateRegionDto {}
+import { IsArray, IsString, MaxLength } from "class-validator";
+import { Region } from "../entities/region.entity";
+import { MaxKey } from "typeorm";
+
+export class CreateRegionDto extends Region {
+    @IsString()
+    @MaxLength(100)
+    regionName: string;
+    @IsArray()
+    regionStates: string[];
+}
