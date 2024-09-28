eb704120863e72bde60be702552a1fb21f17609a
MaksGuz <maax.guzmaan@gmail.com>
2024-09-27 22:15:06 -0600

    Global Auth

---
+++ src/auth/constants/jwt.constants.ts
@@ -0,0 +1,4 @@
+// src/auth/constants/jwt.constants.ts
+
+export const JWT_KEY = 'your-secret-key'; // Cambia esto a una clave más segura
+export const EXPIRES_IN = '1h'; // Cambia esto según tus necesidades
