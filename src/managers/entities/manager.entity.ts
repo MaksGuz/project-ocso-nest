ded01c6b1d28b53bcf28d1fab021161bc3f002a9
MaksGuz <maax.guzmaan@gmail.com>
2024-09-28 20:00:11 -0600

    User relations to manager and employees

--- src/managers/entities/manager.entity.ts
+++ src/managers/entities/manager.entity.ts
@@ -1,5 +1,7 @@
+import { User } from "src/auth/entities/user.entity";
 import { Location } from "src/locations/entities/location.entity";
-import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { JoinAttribute } from "typeorm/query-builder/JoinAttribute";
 
 @Entity()
 export class Manager {
@@ -16,4 +18,10 @@ export class Manager {
     
     @OneToOne(()=> Location)
     location: Location;
+
+    @OneToOne(()=> User)
+    @JoinColumn({
+        name: "userId"
+    })
+    user: User;
 }
