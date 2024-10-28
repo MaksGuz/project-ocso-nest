69a5d8aaf7e4ac4cd13379b7292822a7d28263dc
MaksGuz <maax.guzmaan@gmail.com>
2024-10-27 19:25:36 -0600

    feat: Manager changes for location uploading

--- src/managers/dto/create-manager.dto.ts
+++ src/managers/dto/create-manager.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNumber, IsObject, IsOptional, IsString, MaxLength } from "class-validator";
+import { IsEmail, IsNumber, IsOptional, IsString, MaxLength } from "class-validator";
 import { Manager } from "../entities/manager.entity";
 import { Location } from "src/locations/entities/location.entity";
 import { ApiProperty } from "@nestjs/swagger";
@@ -28,7 +28,7 @@ export class CreateManagerDto extends Manager {
     })
     managerPhoneNumber: string;
     @ApiProperty()
-    @IsObject()
+    @IsNumber()
     @IsOptional()
     location: Location;
 }
